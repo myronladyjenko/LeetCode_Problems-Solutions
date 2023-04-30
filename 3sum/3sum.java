2cc8e33a4eb869a4987112d975f1c0ce5125013d
Myron <86271481+myronladyjenko@users.noreply.github.com>
2023-04-29 22:29:19 -0400

    Time: 48 ms (51.67%) | Memory: 50.5 MB (23.93%) - LeetSync

---
+++ 3sum/3sum.java
@@ -0,0 +1,66 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int l = nums.length;
+        if (l < 3) {
+            return new ArrayList<List<Integer>>();
+        }
+    
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < l; i++) {
+            int numOne = nums[i];
+            int k = i + 1;
+            int m = l - 1;
+
+            while (k < m) {
+                if (i == k) {
+                    k++;
+                    continue;
+                }
+
+                if (i == m) {
+                    m--;
+                    continue;
+                }
+
+                int currSum = numOne + nums[k] + nums[m];
+                if (currSum == 0) {
+                    List<Integer> li = new ArrayList<>();
+
+                    li.add(numOne);
+                    li.add(nums[k]);
+                    li.add(nums[m]);
+
+                    list.add(li);
+                    // Collections.sort(li);
+
+                    // if (!list.contains(li)) {
+                    //     list.add(li);
+                    // }
+
+                    while (k < m && nums[m] == nums[m - 1]) {
+                        m--;
+                    }
+
+                    while (k < m && nums[k] == nums[k + 1]) {
+                        k++;
+                    }
+
+                    k++;
+                    m--;
+                } else if (currSum > 0) {
+                    m--;
+                } else {
+                    k++;
+                } 
+            }
+
+            while (i < nums.length - 1  && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+
+        return list;
+    }
+}
